72d1bfbfe30240d4e9635c1645364f58d5f0bce9
P.Adarsh <adarshsai0788@gmail.com>
2021-01-08 18:25:54 +0530

    these are the changes made by Adarsh

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import "./AdarshApp.css";
+import "./Components/HeaderComponent";
+import HeaderComponents from "./Components/HeaderComponent.jsx";
+import SectionComponent from "./Components/SectionComponent.jsx";
+import SideMenu from "./Components/SideMenu";
+import MyFooter from "./Components/footer";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <SideMenu />
+
+      <div className="App">
+        <header className="App-header">
+          <p>
+            <HeaderComponents />
+          </p>
+        </header>
+        <NavBar />
+
+        <SectionComponent />
+
+        <MyFooter />
+      </div>
     </div>
   );
 }
---
+++ src/Components/FirstChild.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+
+function FirstChild(props) {
+  return <h3>{props.text}</h3>;
+}
+export default FirstChild;
